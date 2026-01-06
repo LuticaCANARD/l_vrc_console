3f5e29a8ad801cd21b805fd96675f5b63eddd5a7
Lutica_CANARD <presan100@gmail.com>
2026-01-06 22:48:30 +0900

    gitignore에 .vscode 추가 및 main 함수 비동기 제거

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ mod integration;
 mod config;
 mod controllers;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     // 터미널 UI 실행
     if let Err(e) = ui::viewer::show_ui() {
         eprintln!("UI 오류: {}", e);
